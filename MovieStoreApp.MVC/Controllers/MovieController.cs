12de70eb19c799328c46b009d25d37421ebd18b7
gc01873 <42818728+gc01873@users.noreply.github.com>
2022-04-17 16:55:06 -0400

    Add files via upload

---
+++ MovieStoreApp.MVC/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieStoreApp.MVC.Repository;
+using MovieStoreApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStoreApp.MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        MovieRepository movies;
+
+        public MovieController()
+        {
+            movies = new MovieRepository();
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<MovieModel> lstmovies = movies.movieList();
+            return View(lstmovies);
+        }
+
+
+        public IActionResult Detail(int Id)
+            
+        {
+            MovieModel movie = movies.find(Id);
+
+            return View(movie);
+
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MovieModel moviemodel)
+        {
+
+            if (moviemodel.Title != null)
+            {
+                
+                return RedirectToAction("Index");
+            }
+            //call the repository insert method to save the movie in database
+            return View(moviemodel);
+        }
+    }
+}
---
+++ MovieStoreApp.MVC/Models/MovieModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieStoreApp.MVC.Models
+{
+    public class MovieModel
+    {
+
+        public MovieModel()
+        {
+
+        }
+
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string PosterUrl { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+
+    }
+
+
+
+}
